230507b75eb87d214a2e325cbb72fa35f56b172d
moizraja70-ai <moizraja70@gmail.com>
2026-02-03 19:27:32 +0500

    Increase auth timeout to 15s to fix permanent loading errors

--- dist/assets/SummaryView-DZqMmKBJ.js
+++ dist/assets/SummaryView-DZqMmKBJ.js
@@ -1,4 +1,4 @@
-import{c as le,r as Fr,j as b,R as Iu,a as M,M as Hr}from"./index-DOnVbQoH.js";import{f as Ur}from"./geminiService-uvdRvENO.js";import{c as Su,v as vr,o as Ee,u as ye,a as Bt,n as et,b as dt,d as xu,t as Ou,E as Yr,e as Ft,f as $e,m as ae,g as jr,h as Wr,i as pe,s as At,r as zr,j as ze,k as ke,l as qr,p as Ht,q as Xt,w as Vt,x as Kt,y as Tt,z as tt,A as Qr,B as Gr,C as Le,D as Lu,F as $t,M as Xr}from"./index-BS89b7ss.js";/**
+import{c as le,r as Fr,j as b,R as Iu,a as M,M as Hr}from"./index-CfjRDgK6.js";import{f as Ur}from"./geminiService-t_rPGGic.js";import{c as Su,v as vr,o as Ee,u as ye,a as Bt,n as et,b as dt,d as xu,t as Ou,E as Yr,e as Ft,f as $e,m as ae,g as jr,h as Wr,i as pe,s as At,r as zr,j as ze,k as ke,l as qr,p as Ht,q as Xt,w as Vt,x as Kt,y as Tt,z as tt,A as Qr,B as Gr,C as Le,D as Lu,F as $t,M as Xr}from"./index-BXqfFE5K.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/geminiService-t_rPGGic.js
+++ dist/assets/geminiService-t_rPGGic.js
@@ -1,4 +1,4 @@
-import{M as I}from"./index-DOnVbQoH.js";var Ao={};/**
+import{M as I}from"./index-CfjRDgK6.js";var Ao={};/**
  * @license
  * Copyright 2025 Google LLC
  * SPDX-License-Identifier: Apache-2.0
