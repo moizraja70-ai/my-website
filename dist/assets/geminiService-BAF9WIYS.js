aa73cd2182f59280fac6fa98e92ae6c6fc855190
moizraja70-ai <moizraja70@gmail.com>
2026-02-03 16:34:36 +0500

    Fix logout: fire and forget without awaiting

--- dist/assets/geminiService-BAF9WIYS.js
+++ dist/assets/geminiService-BAF9WIYS.js
@@ -1,4 +1,4 @@
-import{M as I}from"./index-Blwc9GUf.js";var Ao={};/**
+import{M as I}from"./index-BY4i_tSi.js";var Ao={};/**
  * @license
  * Copyright 2025 Google LLC
  * SPDX-License-Identifier: Apache-2.0
