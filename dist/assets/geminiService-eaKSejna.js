022cefc7701beb30e2086153cb4d5768136a4925
moizraja70-ai <moizraja70@gmail.com>
2026-02-03 18:19:46 +0500

    Verify user exists on server during init

--- dist/assets/geminiService-eaKSejna.js
+++ dist/assets/geminiService-eaKSejna.js
@@ -1,4 +1,4 @@
-import{M as I}from"./index-CKcfFhEV.js";var Ao={};/**
+import{M as I}from"./index-B7rCZKv3.js";var Ao={};/**
  * @license
  * Copyright 2025 Google LLC
  * SPDX-License-Identifier: Apache-2.0
